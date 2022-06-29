e412f4af19e272571ee523dde32bd38343249371
gungunfebrianza <pusatrisetteknologi@gmail.com>
2022-06-29 17:09:07 +0700

    add logging

---
+++ Client-API/Intro/logging.ts
@@ -0,0 +1,20 @@
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "stdout",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
