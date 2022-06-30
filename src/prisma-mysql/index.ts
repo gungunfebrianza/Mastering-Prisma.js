9cd89bf3fbb5f125d6cac9639ebf663c3b67bd5e
gungunfebrianza <pusatrisetteknologi@gmail.com>
2022-06-30 16:08:44 +0700

    major update

--- src/prisma-mysql/index.ts
+++ src/prisma-mysql/index.ts
@@ -3,43 +3,93 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  //CREATE SINGLE RECORD
+  // CREATE SINGLE RECORD
   //   const user = await prisma.user.create({
   //     data: {
-  //       email: "gungunfebrianza@prisma.io",
+  //       email: "gungunfebrianza@cryptolibertarian.id",
   //       name: "Gun Gun Febrianza",
   //     },
   //   });
-
-  //CREATE MULTIPLE RECORD
+  // CREATE MULTIPLE RECORD
   //   const createMany = await prisma.user.createMany({
   //     data: [
-  //       { name: "Gun", email: "gun@prisma.io" },
-  //       { name: "Maudy", email: "maudy@prisma.io" }, // Duplicate unique key!
-  //       { name: "KodokGempal", email: "KodokGempal@prisma.io" },
-  //       { name: "KodokZuma", email: "KodokZuma@prisma.io" },
+  //       { name: "Gun", email: "gun@cryptolibertarian.id" },
+  //       {
+  //         name: "Gun Gun Febrianza",
+  //         email: "gungunfebrianza@cryptolibertarian.id",
+  //       }, // Duplicate unique key!
+  //       { name: "Maudy", email: "maudy@cryptolibertarian.id" },
+  //       { name: "KodokGempal", email: "KodokGempal@cryptolibertarian.id" },
+  //       { name: "KodokZuma", email: "KodokZuma@cryptolibertarian.id" },
   //     ],
   //     skipDuplicates: true,
   //   });
-
-  //FIND ALL USER
-  const listUsers = await prisma.user.findMany();
-  console.dir(listUsers, { depth: null });
-  //FIND ALL USER USING INCLUDE PROFILE
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  });
+  // FIND ALL USER
+  //   const listUsers = await prisma.user.findMany();
+  //   console.dir(listUsers, { depth: null });
+  //   //FIND ALL USER USING INCLUDE PROFILE
+  //   const allUsers = await prisma.user.findMany({
+  //     include: {
+  //       posts: true,
+  //       profile: true,
+  //     },
+  //   });
   //console.dir(allUsers, { depth: null });
-  //FIND SPECIFIC USER
+  // FIND SPECIFIC USER
   //   let resultFindByEmail = await prisma.user
   //     .findUnique({
-  //       where: { email: "gungunfebrianza@prisma.io" },
+  //       where: { email: "gungunfebrianza@cryptolibertarian.id" },
   //     })
   //     .catch((error) => console.log(error));
-  //console.dir(resultFindByEmail);
+  //   console.dir(resultFindByEmail);
+  // FIND BY SPECIFIC CRITERIA
+  //   const findFirstUser = await prisma.user.findFirst({
+  //     where: {
+  //       posts: {
+  //         some: {
+  //           likes: {
+  //             gt: 100,
+  //           },
+  //         },
+  //       },
+  //     },
+  //     orderBy: {
+  //       id: "desc",
+  //     },
+  //   });
+  //   console.dir(findFirstUser);
+  // GET FILTERED LIST OF RECORDS
+  //   const filteredUsers = await prisma.user.findMany({
+  //     where: {
+  //       email: {
+  //         endsWith: "cryptolibertarian.id",
+  //       },
+  //     },
+  //   });
+  //   console.dir(filteredUsers);
+  // GET BY MULTIPLE FIELD VALUES
+  //   const users = await prisma.user.findMany({
+  //     where: {
+  //       OR: [
+  //         {
+  //           name: {
+  //             startsWith: "G",
+  //           },
+  //         },
+  //         {
+  //           AND: {
+  //             profileViews: {
+  //               gt: 0,
+  //             },
+  //             role: {
+  //               equals: "USER",
+  //             },
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   });
+  //   console.dir(users);
   // Run inside `async` function
   // UPDATE SINGLE RECORD
   //   const post = await prisma.user.update({
@@ -105,7 +155,6 @@ async function main() {
   //       authorId: 7,
   //     },
   //   });
-
   //   const deleteUser = prisma.user.delete({
   //     where: {
   //       id: 7,
