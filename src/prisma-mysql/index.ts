9286f5259fb0bc3f26d1631a7ff71a91d8a41722
gungunfebrianza <pusatrisetteknologi@gmail.com>
2022-06-30 15:23:19 +0700

    update multiple record

--- src/prisma-mysql/index.ts
+++ src/prisma-mysql/index.ts
@@ -3,25 +3,42 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  //CREATE SINGLE RECORD
   //   const user = await prisma.user.create({
   //     data: {
   //       email: "gungunfebrianza@prisma.io",
   //       name: "Gun Gun Febrianza",
   //     },
   //   });
+
+  //CREATE MULTIPLE RECORD
+  const createMany = await prisma.user.createMany({
+    data: [
+      { name: "Gun", email: "gun@prisma.io" },
+      { name: "Maudy", email: "maudy@prisma.io" }, // Duplicate unique key!
+      { name: "KodokGempal", email: "KodokGempal@prisma.io" },
+      { name: "KodokZuma", email: "KodokZuma@prisma.io" },
+    ],
+    skipDuplicates: true, // Skip 'Bobo'
+  });
+
+  //FIND ALL USER
+  const listUsers = await prisma.user.findMany();
+  console.dir(listUsers, { depth: null });
+  //FIND ALL USER USING INCLUDE PROFILE
   const allUsers = await prisma.user.findMany({
     include: {
       posts: true,
       profile: true,
     },
   });
-  console.dir(allUsers, { depth: null });
+  //console.dir(allUsers, { depth: null });
   let resultFindByEmail = await prisma.user
     .findUnique({
       where: { email: "gungunfebrianza@prisma.io" },
     })
     .catch((error) => console.log(error));
-  console.dir(resultFindByEmail);
+  //console.dir(resultFindByEmail);
 }
 
 main()
